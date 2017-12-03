4e68cd44b7b7c160b6b98ba6af148d28a8e909a7
DESKTOP-BNH19L5\ianchen <mike840609@gmail.com>
2017-12-03 15:41:06 +0800

    ignore

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Journel_Analytics" 
---
+++ main.py
@@ -0,0 +1,11 @@
+from DocOperator import DocOperator
+
+def main():
+    docOpe_obj = DocOperator("./Journal_Paper/")
+    docOpe_obj.generateDoc()
+    
+
+
+
+if __name__ == '__main__':
+    main()
